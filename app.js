41ca718ca0f92ef137e252db6ae2b8b1d54aee1d
Nadia-Anjum <diaanjum@outlook.dk>
2024-04-11 11:10:36 +0200

    Add CSS styles for the Simple PWA app

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ document
   .addEventListener("click", sendNotification); // Add an event listener to the button with the ID "btn-send-notification"
   
   document
+  .querySelector("#btn-select-contacts")
+  .addEventListener("click", selectContacts); // Add an event listener to the button with the ID "btn-select-contacts"
+
+  document
   .querySelector("#btn-geolocation")
   .addEventListener("click", getGeolocation); // Add an event listener to the button with the ID "btn-geolocation"
 
+
+  // ========== Show Notification ========== //
 
 async function sendNotification() {
   const notificationValue = document.querySelector("#notification").value; // Get the notification message
@@ -44,6 +50,50 @@ async function showNotification(body) {
 }
 
 
+// ========== Select Contacts ========== //
+
+async function selectContacts() {
+    const list = document.querySelector("#contacts"); // Get the contacts element
+  
+    // Check if the Contacts API is available
+    if (!("contacts" in navigator && "ContactsManager" in window)) {
+      list.textContent = "Contacts API is not available";
+      return;
+    }
+  
+    const props = await navigator.contacts.getProperties(); // Get the properties of the contacts
+    const contacts = await navigator.contacts.select(props, {
+      multiple: true
+    }); // Select multiple contacts
+  
+    list.innerHTML = ""; // Clear the contacts element
+  
+    // Create an HTML string with the contacts
+    const html = contacts.reduce((html, contact) => {
+      const names = contact.name.join(", ");
+      const emails = contact.email.join(", ");
+      const telephone = contact.tel.join(", ");
+  
+      return `${html}
+          <p>
+            <span>
+              <strong>${names}</strong><br>
+            </span>
+            <span>
+              ${emails}<br>
+            </span>
+            <span>
+              ${telephone}</p>
+            </span>
+          `;
+    }, ``);
+  
+    list.innerHTML = html; // Set the inner HTML of the contacts element to the HTML string
+  }
+  
+
+
+// ========== Get Geolocation ========== //
 
 async function getGeolocation() {
   const geolocation = document.querySelector("#geolocation"); // Get the geolocation element
